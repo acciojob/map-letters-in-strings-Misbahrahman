c06992ee43d196fe3995cbbd80eff8917fc358a8
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-12-21 19:43:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+
+let word = prompt();
+
+let obj = {};
+function makeObj() {
+	
+	for(let i = 0 ; i < word.length ; i++){
+		let char = word.charAt(i);
+		if(!(char in obj)){
+			obj[char] = [];
+		}
+		
+		obj[char].push(i);
+	}
+
+	return obj;
+}
+
+alert(JSON.stringify(makeObj()));
